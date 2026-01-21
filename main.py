87b1fce5fa1bcdee62ee8b40e6bd1bf8f40ded68
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-01-21 01:40:16 -0600

    added a character counter func

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import count_words
+from stats import count_chars
 
 def main():
     file_path = "books/frankenstein.txt"
     file_content = get_book_text(file_path)
-    counted = count_words(file_content)
-    print(f"Found {counted} total words")
+    counted_words = count_words(file_content)
+    print(f"Found {counted_words} total words")
+    char_dict = count_chars(file_content)
+    print (char_dict)
 
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -1,3 +1,40 @@
 def count_words(file_content):
     num_words = len(file_content.split())
     return num_words
+
+char_dict = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    ".": 0,
+    " ": 0
+}
+def count_chars(file_content):
+    for char in file_content:
+        lowercase_char = char.lower()
+        if lowercase_char in char_dict:
+            char_dict[lowercase_char] += 1
+    return char_dict
