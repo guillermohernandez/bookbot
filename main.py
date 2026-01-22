a55994e723875b47b348fe5ac26d028171b396eb
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-01-22 11:39:57 -0600

    removed hardcoded values in main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import count_words
 from stats import count_chars
 from stats import sorted_dict
+import sys
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]
     file_content = get_book_text(file_path)
     counted_words = count_words(file_content)
     print("============ BOOKBOT ============")
